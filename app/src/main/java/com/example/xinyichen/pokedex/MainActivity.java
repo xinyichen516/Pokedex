884964295d188ba875f0d4920f27a63d65368c3a
Xin Yi Chen <xinyichen516@gmail.com>
2017-09-20 00:22:39 -0700

    committing to work

--- app/src/main/java/com/example/xinyichen/pokedex/MainActivity.java
+++ app/src/main/java/com/example/xinyichen/pokedex/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.xinyichen.pokedex;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        com.example.xinyichen.pokedex.Pokedex pokedex = new Pokedex();
+
+        //TODO Question 1
+        //Create a recyclerview with a linear layout
+
+        RecyclerView rView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        rView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<Company> computerCompanies = new ArrayList<>();
+
+        //TODO Question 2
+        //add 4 companies: apple, asus, dell, and microsoft
+
+        computerCompanies.add(new Company("Apple", 5f, R.drawable.apple));
+        computerCompanies.add(new Company("Asus", 4f, R.drawable.asus));
+        computerCompanies.add(new Company("Dell", 4f, R.drawable.dell));
+        computerCompanies.add(new Company("Microsoft", 4.5f, R.drawable.microsoft));
+
+
+        //TODO Question 3
+        //set the adapter using the constructor
+        RecyclerView.Adapter adapter = new CompaniesAdapter(getApplicationContext(), computerCompanies);
+        rView.setAdapter(adapter);
+
+        //TODO Question 3.5??
+        //guided row_view layout
+
+
     }
 
 
--- app/src/main/java/com/example/xinyichen/pokedex/PokemonAdapter.java
+++ app/src/main/java/com/example/xinyichen/pokedex/PokemonAdapter.java
@@ -1,6 +1,13 @@
 package com.example.xinyichen.pokedex;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -8,13 +15,72 @@ import java.util.ArrayList;
  * Created by xinyichen on 9/19/17.
  */
 
-public class PokemonAdapter {
+public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.CustomViewHolder>{
     Context context;
     ArrayList<Pokedex.Pokemon> pokemonList = new ArrayList<>();
     Pokedex pokedex = new Pokedex();
 
-    public CompaniesAdapter(Context context, ArrayList<Pokedex.Pokemon> companies) {
+    public PokedexAdapter(Context context, ArrayList<Pokedex.Pokemon> companies) {
         this.context = context;
         this.companiesList = new ArrayList<Company>(companies);
+    }
+
+    @Override
+    public CompaniesAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_views, parent, false);
+        return new CustomViewHolder(view);
+    }
+    //TODO Question 6
+    //Bind the data to the holder based on the position
+
+    /* YOUR CODE HERE */
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+        Company company = companiesList.get(position);
+        holder.name.setText(company.companyName);
+        holder.img.setImageResource(company.imageResId);
+        holder.rating.setRating(company.likeCompany);
+            /*@Override
+            public void onClick(View v) {
+            Intent intent = new INtent(context, MainActivity.class);
+            Intent.putExtra(Name)
+            }*/
+    }
+
+
+    //TODO Question 7
+    //Override the item size method
+
+    @Override
+    public int getItemCount() {
+        return companiesList.size();
+    }
+
+    //TODO Question 8
+    //Create a CustomViewHolder class that extends the base RecyclerView.ViewHolder
+    //Override its constructor class with the following signature:
+    //CustomViewHolder(View view) {
+    //    super(view);
+    //    ...
+    //}
+    //and create instance variables for the UI elements in the layout file
+    //Hint: findViewById is a method of the View class
+
+    /* YOUR CODE HERE */
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        RatingBar rating;
+        ImageView img;
+        TextView name;
+
+        CustomViewHolder(View view) {
+            super(view);
+            this.name = (TextView) view.findViewById(R.id.companyName);
+            this.rating = (RatingBar) view.findViewById(R.id.companyRating);
+            this.img = (ImageView) view.findViewById(R.id.companyPic);
+        }
+
     }
 }
