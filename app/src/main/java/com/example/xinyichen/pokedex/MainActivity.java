051d8b00724761ae9abd0ba87bd8f6753b2d7f5c
Kaden Dippe <kadendippe@berkeley.edu>
2017-09-21 00:59:27 -0700

    layout switcher

--- app/src/main/java/com/example/xinyichen/pokedex/MainActivity.java
+++ app/src/main/java/com/example/xinyichen/pokedex/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.xinyichen.pokedex;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 import com.example.xinyichen.pokedex.Pokedex.Pokemon;
 
 import java.util.ArrayList;
 
+import static com.example.xinyichen.pokedex.R.id.fButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //selects wether the layout is linear or not
+    static boolean linearLayout = true;
+
+    RecyclerView rView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +30,34 @@ public class MainActivity extends AppCompatActivity {
         //TODO Question 1
         //Create a recyclerview with a linear layout
 
-        RecyclerView rView = (RecyclerView) findViewById(R.id.recyclerView);
+        rView = (RecyclerView) findViewById(R.id.recyclerView);
 
         rView.setLayoutManager(new LinearLayoutManager(this));
         Pokedex pokedex = new Pokedex();
+
+        //floating button
+        FloatingActionButton layoutButton = (FloatingActionButton) findViewById(R.id.fButton);
+
+        //change the layout and icon when the button is clicked
+        layoutButton.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(linearLayout){
+                    rView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+                    linearLayout = false;
+                } else{
+                    rView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+                    linearLayout = true;
+                }
+
+            }
+        });
+
+
+
 
         ArrayList<Pokedex.Pokemon> pokemonList = pokedex.getPokemon();
 
