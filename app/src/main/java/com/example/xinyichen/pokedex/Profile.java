cc70618efe71c61c1c10dc41566f260f92ec7d75
Xin Yi Chen <xinyichen516@gmail.com>
2017-09-21 23:11:48 -0700

    finished profile

---
+++ app/src/main/java/com/example/xinyichen/pokedex/Profile.java
@@ -0,0 +1,91 @@
+package com.example.xinyichen.pokedex;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.xinyichen.pokedex.Pokedex.Pokemon;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Profile extends AppCompatActivity {
+
+    Pokemon pokemon = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //need an intent that tells you what pokemon you are on, an index
+        //for now, just gonna use abomasnow
+        Intent intent = getIntent();
+        String pName = intent.getStringExtra("name");
+
+        //so I can use getPokemon()
+        Pokedex p = new Pokedex();
+
+        //pokemon arraylist
+        ArrayList<Pokemon> pokeList = p.getPokemon();
+
+        //current pokemon
+        for (Pokemon poke : pokeList) {
+            if (poke.name.equals(pName)) {
+                pokemon = poke;
+            }
+        }
+
+        //picture
+        ImageView Image = (ImageView) findViewById(R.id.imageView);
+
+        //set picture with glide library
+        Context context = getApplicationContext();
+        Glide.with(context)
+                .load("http://assets.pokemon.com/assets/cms2/img/pokedex/full/" + pokemon.number + ".png" )
+                .into(Image);
+
+        //name
+        TextView name = (TextView) findViewById(R.id.name);
+        //settin name text
+        name.setText(pokemon.name);
+
+        //stats
+        TextView stats = (TextView) findViewById(R.id.Stats);
+        //setting stats text
+        stats.setText("Number : " + pokemon.number +
+                "\n Attack: " + pokemon.attack +
+                "\n Defense: " + pokemon.defense +
+                "\n Hp: " + pokemon.hp +
+                "\n species: " + pokemon.species);
+
+        //websearch button
+        Button webButton = (Button) findViewById(R.id.button);
+
+
+
+        //creating websearch onclick intent
+        webButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String URL = "http://www.google.com/#q=" + pokemon.name;
+                Uri uri = Uri.parse(URL);
+                //websearch intent
+                Intent webSearch = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(webSearch);
+            }
+        });
+
+    }
+
+    //https://tutorialwing.com/android-glide-library-tutorial-example/ glide library tutorial
+
+
+}
